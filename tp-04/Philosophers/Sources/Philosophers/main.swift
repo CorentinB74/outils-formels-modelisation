fdbbd6e82fef7d04a414727821c4680dfbec3dd3
CorentinB74 <corentin.bugnot@etu.unige.ch>
2017-11-19 20:55:37 +0100

    Add an example

--- tp-04/Philosophers/Sources/Philosophers/main.swift
+++ tp-04/Philosophers/Sources/Philosophers/main.swift
@@ -49,3 +49,51 @@ do {
         print(m)
     }
 }
+
+do {
+  // dom(Ingredients)
+  enum Ingredients {
+  case p,t,m
+  }
+  // dom(Smokers)
+  enum Smokers {
+  case mia, bob, tom
+  }
+  // dom(Referee)
+  enum Referee {
+  case rob
+  }
+
+  // Types = { Ingredients, Smokers, Referee}
+  enum Types {
+  case ingredients(Ingredients)
+  case smokers(Smokers)
+  case referee(Referee)
+  }
+
+  let s = PredicateTransition<Types>(
+      preconditions: [
+          PredicateArc(place: "i", label: [.variable("x"), .variable("y")]),
+          PredicateArc(place: "s", label: [.variable("s")]),
+      ],
+      postconditions: [
+          PredicateArc(place: "r", label: [.function({ _ in .referee(.rob)})]),
+          PredicateArc(place: "w", label: [.variable("s")]),
+      ],
+      conditions    : [{ binding in
+          // Retrieve the indices of the philosopher and forks from the binding.
+          guard case let .smokers(s) = binding["s"]!,
+                case let .ingredients(x)      = binding["x"]!,
+                case let .ingredients(y)       = binding["y"]!
+          else {
+              return false
+          }
+
+          switch (s,x,y) {
+          case (.mia, .p, .t): return true
+          case (.tom, .p, .m): return true
+          case (.bob, .t, .m): return true
+          default: return false
+          }
+      }])
+}
